712f4610f65d10971c800f68bc0dbb074b4dc568
Tle bingo <asrattilahunasrat@gmail.com>
2026-08-30 14:09:33 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tlebirr.app"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
